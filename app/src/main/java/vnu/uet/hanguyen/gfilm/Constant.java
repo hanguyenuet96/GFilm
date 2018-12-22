02969d1977b3c557985e4b5cdcead52c4a4cb81b
Nguyên Hạ <25699875+hanguyenuet96@users.noreply.github.com>
2018-12-22 23:46:52 +0700

    first commit

---
+++ app/src/main/java/vnu/uet/hanguyen/gfilm/Constant.java
@@ -0,0 +1,6 @@
+package vnu.uet.hanguyen.gfilm;
+
+
+public interface Constant {
+    String IP_SERVER = "https://onfilm.000webhostapp.com";
+}
---
+++ app/src/main/java/vnu/uet/hanguyen/gfilm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,34 @@
+package vnu.uet.hanguyen.gfilm;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
+
+
+
+    @Override
+    public void onTokenRefresh() {
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        registerToken(recent_token);
+    }
+
+    public void registerToken(String recent_token) {
+        OkHttpClient httpClient = new OkHttpClient();
+        RequestBody body = new FormBody.Builder().add("Token",recent_token).build();
+        Request request = new Request.Builder().url("https://onfilm.000webhostapp.com/OnFilm/notification/register.php").post(body).build();
+        try {
+            httpClient.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
